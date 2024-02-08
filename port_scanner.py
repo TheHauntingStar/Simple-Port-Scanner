3fc67dce764d2aba852f929ffcb6333a6b08ca38
TheHauntingStar <152018933+TheHauntingStar@users.noreply.github.com>
2024-02-08 13:44:41 +0400

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,27 @@
+import socket
+
+def scan_ports(target_ip, target_ports):
+    open_ports = []
+
+    for port in target_ports:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+
+            try:
+                sock.connect((target_ip, port))
+                open_ports.append(port)
+            except (socket.timeout, socket.error):
+                pass
+
+    return open_ports
+
+if __name__ == "__main__":
+    target_ip = "37.19.205.196"  # Ip Address here
+    ports_to_scan = [53, 80, 58, 193, 22, 21, 96, 65535, 399] 
+
+    open_ports = scan_ports(target_ip, ports_to_scan)
+
+    if open_ports:
+        print(f"Open ports on {target_ip}: {open_ports}")
+    else:
+        print(f"No open ports found on {target_ip}")
